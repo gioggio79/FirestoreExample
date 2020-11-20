4b1878dac79454e760610d4a0198528f1eddf6ce
Giorgio Baroni <g.baroni@melazeta.com>
2020-11-20 10:20:05 +0100

    First commit

---
+++ FirestoreExample.Android/Implementations/FirebaseImplementation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Firestore;
+using FirestoreExample.Droid.Implementations;
+using FirestoreExample.Interfaces;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FirebaseImplementation))]
+namespace FirestoreExample.Droid.Implementations
+{
+	public class FirebaseImplementation : IFirebase
+	{
+		public void InitFirebase()
+		{
+			var FirestoreIstance = FirebaseFirestore.GetInstance(MainActivity.MyFirebaseApp);
+			//var FirestoreIstance = FirebaseFirestore.Instance;
+			if (FirestoreIstance != null)
+			{
+				//DO SOMETHING
+			}
+
+		}
+	}
+}
---
+++ FirestoreExample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using FirestoreExample.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirestoreExample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void FirebaseInit(object sender, EventArgs e)
+		{
+			DependencyService.Get<IFirebase>().InitFirebase();
+		}
+	}
+}
